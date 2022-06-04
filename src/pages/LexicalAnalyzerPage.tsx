b3b66770e796586d62cf8df2c6872730a56da9a8
Erickson Manuel Holguin <erickson01d@gmail.com>
2022-06-04 09:36:05 -0400

    Adding detected variable

--- src/pages/LexicalAnalyzerPage.tsx
+++ src/pages/LexicalAnalyzerPage.tsx
@@ -4,11 +4,33 @@ import { Box, Button, Divider, chakra, HStack, Stack } from "@chakra-ui/react";
 import { Editor } from "../components/Editor";
 import { useForm } from "react-hook-form";
 
-export const LexicalAnalyzerPage = () => {
-  const { control, handleSubmit } = useForm();
+type OnSubmit = {
+  LexicalAnalyzerEditor: string;
+};
 
-  function onSubmit(values: any) {
-    console.log(values.LexicalAnalyzerEditor.split("\r\n"));
+export const LexicalAnalyzerPage = () => {
+  const { control, handleSubmit } = useForm<OnSubmit>();
+
+  function onSubmit(values: OnSubmit) {
+    const code = values.LexicalAnalyzerEditor.split("\r\n").map((line, i) => ({
+      lineNumber: i + 1,
+      content: line,
+    }));
+
+    const result = code.map((line) => {
+      let token = null;
+      if (isVariableRegex.test(line.content)) {
+        token = detectedVariableType(line.content, line.lineNumber);
+      }
+      return token;
+    });
+
+    if (result.includes(null)) {
+      console.log("Hay un error en la línea " + (result.indexOf(null) + 1));
+      return;
+    }
+
+    console.table(result);
   }
 
   return (
@@ -30,8 +52,52 @@ export const LexicalAnalyzerPage = () => {
         id="lexical-analyzer-form"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <Editor name="LexicalAnalyzerEditor" control={control} />
+        <Editor name="LexicalAnalyzerEditor" control={control as any} />
       </chakra.form>
     </Stack>
   );
 };
+
+const detectedVariableType = (token: string, lineNumber: number) => {
+  let variableType = null;
+  let value: string | number = token.split(":=")[1].trim();
+
+  if (/^string/.test(token)) {
+    if (!/^'.*'$/.test(value)) {
+      console.log("Error: El valor ingresado no es un string");
+    }
+    variableType = "string";
+    value = value.replace(/'/g, "");
+  } else if (/^number/.test(token)) {
+    Number.isNaN(Number(value)) &&
+      console.log("Error: El valor ingresado no es un numero");
+    variableType = "number";
+    value = Number(value);
+  } else if (/^date/.test(token)) {
+    variableType = "date";
+  } else if (/^bool/.test(token)) {
+    variableType = "bool";
+    if (!["y", "n"].includes(value)) {
+      console.log("Error: El valor ingresado no es un booleano");
+      return;
+    }
+    variableType = "bool";
+  }
+
+  return {
+    variableType,
+    token,
+    value,
+    lineNumber,
+    identifier: token.split(" ")[1].trim(),
+  };
+};
+
+enum variablesType {
+  "string",
+  "number",
+  "date",
+  "bool",
+}
+
+const isVariableRegex = /\b(string|number|date|bool)\b ([^\s]+) := .*/;
